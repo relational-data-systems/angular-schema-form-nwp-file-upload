40831c930cee84c03efc2530da41b11e32187589
Keith Lin <keith@relational.com.au>
2017-05-09 17:37:48 +0930

    revise gulp build script

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -4,6 +4,7 @@ var addStream = require('add-stream');
 var angularTemplatecache = require('gulp-angular-templatecache');
 var concat = require('gulp-concat');
 var connect = require('gulp-connect');
+var del = require('del');
 var header = require('gulp-header');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
@@ -50,6 +51,10 @@ gulp.task('build-app-prod', function () {
 });
 
 gulp.task('default', ['build-app-dev', 'build-app-prod']);
+
+gulp.task('clean', function () {
+  return del(['./dist'], {dot: true});
+});
 
 gulp.task('watch', function () {
   gulp.watch('./src/**/*', ['default']);
